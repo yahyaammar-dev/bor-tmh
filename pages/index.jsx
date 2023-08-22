86cd3d416278102a9e14884d0dc46a25296c9074
yahyaammar-dev <yahyaammar4807@gmail.com>
2023-08-22 12:55:11 +0500

    added button to root

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ export default function App() {
         <option value="it">it</option>
       </select> */}
         <div className="App">
-          <button onClick={()=>{nexiPayByLink()}}></button>
+          <button onClick={()=>{nexiPayByLink()}}>Test me</button>
           <Booking /> 
         </div>
     </IntlProvider>
